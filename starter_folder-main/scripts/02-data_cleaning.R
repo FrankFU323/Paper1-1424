bf998c83ea123ce854f01f88e066f18bd916dec2
FrankFU323 <tianrui.fu@mail.utoronto.ca>
2024-09-24 14:34:54 -0400

    Update 02-data_cleaning.R
    remove the outliers

--- starter_folder-main/scripts/02-data_cleaning.R
+++ starter_folder-main/scripts/02-data_cleaning.R
@@ -15,13 +15,20 @@ raw_data <- read_csv("data/raw_data/pre_data.csv")
 
 cleaned_data <- raw_data |>
   janitor::clean_names() |> 
-  select(beach_name, collection_date, e_coli) |> 
+  select(beach_name, collection_date, site_name, e_coli) |> 
   mutate(
     e_coli = as.numeric(e_coli),
     Date = lubridate::ymd(collection_date)
   ) |> 
   rename(Name = beach_name) |> 
   tidyr::drop_na()
+
+threshold <- 2000
+cleaned_data <- cleaned_data %>%
+  mutate(is_outlier = ifelse(e_coli > threshold, "Outlier", "Normal"))
+
+cleaned_data <- cleaned_data %>%
+  filter(e_coli <= threshold)
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
